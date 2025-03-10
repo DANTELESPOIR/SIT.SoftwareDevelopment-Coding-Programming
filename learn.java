5b2b3313b027375e380507d589da3075046c9961
Dante Lespoir <danylespoir@gmail.com>
2025-03-10 19:26:36 +0400

    lean

---
+++ learn.java
@@ -0,0 +1,98 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World (on the same line)");
+        System.out.println("This will print it on another line");
+        System.out.print("Hello ");
+        System.out.print("World");
+        System.out.println("The Hello world above are on the same line but they are not onj the same print");
+        System.out.println(100 + " The value is not a string (words) but an integer - Number");
+        System.out.println(15.5 + " This here is also not a tring but a Float (decimal numbers?)");
+//-------------------------------------------------------------------------
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("Hi I am learning java");
+        System.out.println("It is interesting");
+        System.out.println("I am willing to learn fast");
+        
+        System.out.println("-----------------------------------------------------------------");
+        System.out.print("Hi");
+        System.out.print("");
+        System.out.print(" I");
+        System.out.print("");
+        System.out.print(" am");
+        System.out.print("");
+        System.out.print(" Learning");
+        System.out.print("");
+        System.out.print(" JAVA");
+        System.out.print("");
+        System.out.println("");
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("   *   ");
+        System.out.println(" *   * ");
+        System.out.println("* * * *") ;
+        System.out.println("*     *");
+        System.out.println("*     *");
+        
+        System.out.println("-----------------------------------------------------------------");
+       
+        System.out.println("* * * *");
+        System.out.println(" *      *");
+        System.out.println(" * * * *");
+        System.out.println(" *      *");
+        System.out.println("* * * *");
+        
+        System.out.println("-----------------------------------------------------------------");
+       
+        System.out.println("  * * * * ");
+        System.out.println("*  ");
+        System.out.println("*  ");
+        System.out.println("*  ");
+        System.out.println("  * * * * ");
+
+        byte numONE = -128;
+        short numTWO = 127;
+        int numTHREE =30;
+        long numFOUR = 40;
+        float numFIVE = 5;
+        double numSIX= 60.012;
+
+        System.out.println(numONE);
+        System.out.println(numTWO);
+        System.out.println(numTHREE);
+        System.out.println(numFOUR);
+        System.out.println(numFIVE);
+        System.out.println(numSIX);
+
+        int words =  2*( 2 * 63);
+
+        System.out.println(words);
+
+        char Alpha = 'B';
+        
+        System.out.println(Alpha);
+
+        String A = "Bread";
+        
+        
+        System.out.println(A);
+
+        boolean Tree = true;
+
+        System.out.println(Tree);
+
+    
+    } //hello world  System.out.println("");
+}
+/*This is used to write a very long multi lined summary? or long text if needed for example
+maybe an explanation of how the code works with other code of where else it is used */
+
+//To print or debug anything the values and other validation can be done using print
+//Here is an example System.out.println() - this will print what ever value you have written 
